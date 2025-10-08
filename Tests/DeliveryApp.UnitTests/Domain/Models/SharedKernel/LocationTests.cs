f968a2d5bb9ee3b620e70c78dec2242288f7cd4c
i.petrov <ipb.tech@yandex.ru>
2025-10-08 23:29:29 +0300

    module-2

---
+++ Tests/DeliveryApp.UnitTests/Domain/Models/SharedKernel/LocationTests.cs
@@ -0,0 +1,73 @@
+﻿using DeliveryApp.Core.Domain.Models.SharedKernel;
+using FluentAssertions;
+using Xunit;
+
+namespace DeliveryApp.UnitTests.Domain.Models.SharedKernel;
+
+public class LocationTests
+{
+    [Fact]
+    public void SuccessCreatedWithRandom()
+    {
+        //Arrange
+
+        //Act
+        var location = Location.CreateRandom();
+
+        //Assert
+        location.IsSuccess.Should().BeTrue();
+        location.Value.X.Should().BeLessThanOrEqualTo(10);
+        location.Value.Y.Should().BeLessThanOrEqualTo(10);
+    }
+
+    [Theory]
+    [InlineData(1, 1)]
+    [InlineData(1, 7)]
+    [InlineData(8, 5)]
+    [InlineData(10, 1)]
+    [InlineData(10,10)]
+    public void SuccessCreatedWithParameters(int x, int y)
+    {
+        //Arrange
+
+        //Act
+        var location = Location.Create(x, y);
+
+        //Assert
+        location.IsSuccess.Should().BeTrue();
+        location.Value.X.Should().Be(x);
+        location.Value.Y.Should().Be(y);
+    }
+
+    [Theory]
+    [InlineData(-1, 1)]
+    [InlineData(0, 0)]
+    [InlineData(11, 5)]
+    [InlineData(21, 33)]
+    [InlineData(10, 0)]
+    public void FailedCreatedWithInvalidParameters(int x, int y)
+    {
+        //Arrange
+
+        //Act
+        var location = Location.Create(x, y);
+
+        //Assert
+        location.IsSuccess.Should().BeFalse();
+    }
+
+    [Fact]
+    public void SuccessCalculateDistanceBetween()
+    {
+        //Arrange
+        var max = Location.Max;
+        var min = Location.Min;
+
+        //Act
+        var distance = min.DistanceBetween(max);
+
+        //Assert
+        distance.IsSuccess.Should().BeTrue();
+        distance.Value.Should().Be(18);
+    }
+}
