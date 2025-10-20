e681cf67f8816b9f5c437fad6f503ffa515545ce
Ilya Petrov <ipb.tech@yandex.ru>
2025-10-20 22:26:39 +0300

    Module 4 (#4)
    * storage place entity
    
    * tests
    
    * location comments
    
    * order aggregate
    
    * courier aggregate
    
    * courier tests
    
    * order tests
    
    * gitkeep
    
    * s

---
+++ DeliveryApp.Core/Domain/Models/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,44 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace DeliveryApp.Core.Domain.Models.OrderAggregate;
+
+/// <summary>
+/// Статус заказа
+/// </summary>
+public class OrderStatus : ValueObject
+{
+    /// <summary>
+    /// Создан
+    /// </summary>
+    public static OrderStatus Created => new(nameof(Created).ToLowerInvariant());
+
+    /// <summary>
+    /// Назначен
+    /// </summary>
+    public static OrderStatus Assigned => new(nameof(Assigned).ToLowerInvariant());
+
+    /// <summary>
+    /// Выполнен
+    /// </summary>
+    public static OrderStatus Completed => new(nameof(Completed).ToLowerInvariant());
+
+    // ctr for EF Core
+    private OrderStatus()
+    { }
+
+    private OrderStatus(string name) : this()
+    {
+        Name = name;
+    }
+
+    /// <summary>
+    /// Название
+    /// </summary>
+    public string Name { get; }
+
+    /// <inheritdoc />
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Name;
+    }
+}
