eaf715cafab1ad9f74d8ad5d9ae81ef387d5200b
i.petrov <ipb.tech@yandex.ru>
2025-10-29 16:21:47 +0300

    impl

---
+++ DeliveryApp.Infrastructure/Adapters/Postgres/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using DeliveryApp.Core.Domain.Models.CourierAggregate;
+using DeliveryApp.Core.Domain.Models.OrderAggregate;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryApp.Infrastructure.Adapters.Postgres;
+
+/// <summary>
+/// Контекст базы данных
+/// </summary>
+public class ApplicationDbContext : DbContext
+{
+    /// <inheritdoc />
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    { }
+        
+    /// <summary>
+    /// Заказы
+    /// </summary>
+    public DbSet<Order> Orders { get; set; }
+
+    /// <summary>
+    /// Курьеры
+    /// </summary>
+    public DbSet<Courier> Couriers { get; set; }
+
+    /// <inheritdoc />
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
---
+++ DeliveryApp.Infrastructure/Adapters/Postgres/EntityConfigurations/CourierAggregate/CourierEntityTypeConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using DeliveryApp.Core.Domain.Models.CourierAggregate;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DeliveryApp.Infrastructure.Adapters.Postgres.EntityConfigurations.CourierAggregate;
+
+internal class CourierEntityTypeConfiguration : IEntityTypeConfiguration<Courier>
+{
+    public void Configure(EntityTypeBuilder<Courier> entityTypeBuilder)
+    {
+        entityTypeBuilder.ToTable("couriers");
+
+        entityTypeBuilder.HasKey(entity => entity.Id);
+
+        entityTypeBuilder
+            .Property(entity => entity.Id)
+            .ValueGeneratedNever()
+            .HasColumnName("id")
+            .IsRequired();
+
+        entityTypeBuilder
+            .Property(entity => entity.Name)
+            .HasColumnName("name")
+            .IsRequired();
+        
+        entityTypeBuilder
+            .Property(entity => entity.Speed)
+            .HasColumnName("speed")
+            .IsRequired();
+
+        entityTypeBuilder
+            .OwnsOne(entity => entity.Location, l =>
+            {
+                l.Property(x => x.X).HasColumnName("location_x").IsRequired();
+                l.Property(y => y.Y).HasColumnName("location_y").IsRequired();
+                l.WithOwner();
+            });
+
+        entityTypeBuilder.Navigation(entity => entity.Location).IsRequired();
+
+        entityTypeBuilder.Navigation(entity => entity.Location).AutoInclude();
+        entityTypeBuilder.Navigation(entity => entity.StoragePlaces).AutoInclude();
+    }
+}
