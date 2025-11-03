5ea652ba617fb69cdcf7da1a0e6098107794770a
i.petrov <ipb.tech@yandex.ru>
2025-11-03 23:13:54 +0300

    create courier use case

---
+++ DeliveryApp.Core/Application/Commands/CreateCourier/CreateCourierHandler.cs
@@ -0,0 +1,31 @@
+﻿using CSharpFunctionalExtensions;
+using Dapper;
+using MediatR;
+using Npgsql;
+using Primitives;
+using System.Data;
+using DeliveryApp.Core.Domain.Models.CourierAggregate;
+using DeliveryApp.Core.Domain.Models.SharedKernel;
+using DeliveryApp.Core.Ports.Repositories;
+
+namespace DeliveryApp.Core.Application.Commands.CreateCourier;
+
+/// <summary>
+/// Обработчик для <see cref="CreateCourierCommand"/>
+/// </summary>
+public class CreateCourierHandler(
+    IUnitOfWork unitOfWork,
+    ICourierRepository courierRepository) : IRequestHandler<CreateCourierCommand, UnitResult<Error>>
+{
+    /// <inheritdoc />
+    public async Task<UnitResult<Error>> Handle(CreateCourierCommand message, CancellationToken cancellationToken)
+    {
+        var location = Location.CreateRandom().Value;
+        var courier = Courier.Create(message.Name, message.Speed, location).Value;
+
+        await courierRepository.AddAsync(courier);
+        await unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return UnitResult.Success<Error>();
+    }
+}
