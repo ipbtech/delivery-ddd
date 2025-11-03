dc8a9a60a8cdfa8a03ad264194bdf7cf1e310650
i.petrov <ipb.tech@yandex.ru>
2025-11-03 17:36:33 +0300

    program.cs

--- DeliveryApp.Api/Program.cs
+++ DeliveryApp.Api/Program.cs
@@ -4,6 +4,12 @@ using DeliveryApp.Core.Ports.Repositories;
 using DeliveryApp.Infrastructure.Adapters.Postgres;
 using DeliveryApp.Infrastructure.Adapters.Postgres.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
+using OpenApi.Filters;
+using OpenApi.Formatters;
+using OpenApi.OpenApi;
 using Primitives;
 using System.Reflection;
 
@@ -18,7 +24,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(
         policy =>
         {
-            policy.AllowAnyOrigin(); // Не делайте так в проде!
+            policy.AllowAnyOrigin();
         });
 });
 
@@ -47,6 +53,43 @@ builder.Services.AddScoped<IOrderRepository, OrderRepository>();
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
 
+// HTTP Handlers
+builder.Services.AddControllers(options =>
+    {
+        options.InputFormatters.Insert(0, new InputFormatterStream());
+    })
+    .AddNewtonsoftJson(options =>
+    {
+        options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+        options.SerializerSettings.Converters.Add(new StringEnumConverter
+        {
+            NamingStrategy = new CamelCaseNamingStrategy()
+        });
+    });
+
+
+// Swagger
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("1.0.0", new OpenApiInfo
+    {
+        Title = "Delivery Service",
+        Description = "Отвечает за диспетчеризацию доставки",
+        Contact = new OpenApiContact
+        {
+            Name = "Kirill Vetchinkin",
+            Url = new Uri("https://microarch.ru"),
+            Email = "info@microarch.ru"
+        }
+    });
+    options.CustomSchemaIds(type => type.FriendlyId(true));
+    options.IncludeXmlComments(
+        $"{AppContext.BaseDirectory}{Path.DirectorySeparatorChar}{Assembly.GetEntryAssembly()?.GetName().Name}.xml");
+    options.DocumentFilter<BasePathFilter>("");
+    options.OperationFilter<GeneratePathParamsValidationFilter>();
+});
+builder.Services.AddSwaggerGenNewtonsoftSupport();
+
 var app = builder.Build();
 
 // -----------------------------------
@@ -58,6 +101,20 @@ else
 
 app.UseHealthChecks("/health");
 app.UseRouting();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseSwagger(c => { c.RouteTemplate = "openapi/{documentName}/openapi.json"; })
+    .UseSwaggerUI(options =>
+    {
+        options.RoutePrefix = "openapi";
+        options.SwaggerEndpoint("/openapi/1.0.0/openapi.json", "Swagger Delivery Service");
+        options.RoutePrefix = string.Empty;
+        options.SwaggerEndpoint("/openapi-original.json", "Swagger Delivery Service");
+    });
+
+app.UseCors();
+app.MapControllers();
 
 // Apply Migrations
 using (var scope = app.Services.CreateScope())
